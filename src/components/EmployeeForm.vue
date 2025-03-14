fdf45a3b4c81d2c0eb2dd2fd30f35132a9af947c
alejandrojuarez117 <aljuarez117@gmail.com>
2025-03-13 19:56:04 -0600

    add error handling

--- src/components/EmployeeForm.vue
+++ src/components/EmployeeForm.vue
@@ -2,9 +2,22 @@
   <div id="employee-form">
     <form @submit.prevent="handleSubmit">
       <label>Employee Name</label>
-      <input v-model="employee.name" type="text" />
+      <input 
+        :class="{ 'has-error': submitting && invalidName }"
+        v-model="employee.name" 
+        type="text"
+        @focus="clearStatus"
+        @keypress="clearStatus"
+      />
       <label>Employee Email</label>
-      <input v-model="employee.email" type="text" />
+      <input
+        :class="{ 'has-error': submitting && invalidEmail }"
+        v-model="employee.email" 
+        type="text"
+        @focus="clearStatus"
+      />
+      <p v-if="error && submitting" class="error-message">! Please fill out all required fields</p>
+      <p v-if="success" class="success-message">✅ Employee successfully added</p>
       <button>Add Employee</button>
     </form>
   </div>
@@ -16,14 +29,46 @@
     data() {
       return {
         employee: {
-          name: '',
-          email: '',
+          submitting: false,
+          error: false,
+          sucess: false,
+            name: '',
+            email: '',
         },
       }
     },
     methods: {
       handleSubmit() {
+        this.submitting = true
+        this.clearStatus()
+
+        if (this.invalidName || this.invalidEmail) {
+          this.error = true
+          return
+        }
+
         this.$emit('add:employee', this.employee)
+        this.employee = {
+          name: '',
+          email: '',
+        }
+        this.error = false
+        this.success = true
+        this.submitting = false
+      },
+
+      clearStatus() {
+        this.success = false
+        this.error = false
+      }
+    },
+    computed: {
+      invalidName() {
+        return this.employee.name === ''
+      },
+
+      invalidEmail() {
+        return this.employee.email === ''
       },
     },
   }
@@ -32,5 +77,17 @@
 <style scoped>
   form {
     margin-bottom: 2rem;
+  }
+
+  [class*='-message'] {
+    font-weight: 500;
+  }
+
+  .error-message {
+    color: #d33c40;
+  }
+
+  .success-message {
+    color: #32a95d;
   }
 </style>
